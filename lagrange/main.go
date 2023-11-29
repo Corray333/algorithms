60688a43ed1870a0a689d30afc5ce92e41ea7194
Corray333 <mark.corray.off@gmail.com>
2023-11-29 19:03:15 +0300

    Changed interface a bit

--- lagrange/main.go
+++ lagrange/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./public")))
-	http.ListenAndServe("127.0.0.1:3000", nil)
+	fmt.Println("Server is listening 3001...")
+	http.ListenAndServe("127.0.0.1:3001", nil)
 }
